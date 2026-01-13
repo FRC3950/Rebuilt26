82c349e25325a4419c377a040ecf3528406efb54
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-01-13 17:02:44 -0500

    yayyayayyayayyay. first real thingy 2026 yyayayyay.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Rebuilt26'";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "9353f3827c015d4c45a8adeb13131c30f5bcd203";
+  public static final String GIT_DATE = "2026-01-13 16:36:56 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-13 17:02:16 EST";
+  public static final long BUILD_UNIX_TIME = 1768341736378L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
