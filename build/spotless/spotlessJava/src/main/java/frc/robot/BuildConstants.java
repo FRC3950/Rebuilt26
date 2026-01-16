be44821289b9864585be5bf44acc99c9925bb399
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-01-16 17:24:52 -0500

    fix building issue and gradle. wpilib broke = fix

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26'";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "165e10b7cc5482ca03a4841006769d7646c29e5e";
-  public static final String GIT_DATE = "2026-01-13 17:12:01 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "e75366c030f5973966b0b06d8bd25386f409020a";
+  public static final String GIT_DATE = "2026-01-13 17:23:39 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-13 17:17:25 EST";
-  public static final long BUILD_UNIX_TIME = 1768342645001L;
+  public static final String BUILD_DATE = "2026-01-16 17:24:07 EST";
+  public static final long BUILD_UNIX_TIME = 1768602247566L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
