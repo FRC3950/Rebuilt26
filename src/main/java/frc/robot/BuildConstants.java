f8573fe29c0d95b04d59ecbf124f564b69286969
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-01-16 19:40:24 -0500

    Added in Vision template functionality. Needs to be configured for final robot but barebones works for now.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26'";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "e75366c030f5973966b0b06d8bd25386f409020a";
-  public static final String GIT_DATE = "2026-01-13 17:23:39 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-16 17:24:07 EST";
-  public static final long BUILD_UNIX_TIME = 1768602247566L;
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "e461cbeb466fafa33eace9746455f252adc483ec";
+  public static final String GIT_DATE = "2026-01-16 18:02:25 EST";
+  public static final String GIT_BRANCH = "Vision";
+  public static final String BUILD_DATE = "2026-01-16 19:38:56 EST";
+  public static final long BUILD_UNIX_TIME = 1768610336465L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
