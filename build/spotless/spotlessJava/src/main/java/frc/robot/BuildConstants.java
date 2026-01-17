b7e27fea19998de1807f9bc316bd70d891006dc8
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-01-17 17:37:46 -0500

    updated vision april; tag map for 2026. ready for merge.

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26'";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "e461cbeb466fafa33eace9746455f252adc483ec";
-  public static final String GIT_DATE = "2026-01-16 18:02:25 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "f8573fe29c0d95b04d59ecbf124f564b69286969";
+  public static final String GIT_DATE = "2026-01-16 19:40:24 EST";
   public static final String GIT_BRANCH = "Vision";
-  public static final String BUILD_DATE = "2026-01-16 19:38:56 EST";
-  public static final long BUILD_UNIX_TIME = 1768610336465L;
+  public static final String BUILD_DATE = "2026-01-17 17:17:51 EST";
+  public static final long BUILD_UNIX_TIME = 1768688271245L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
