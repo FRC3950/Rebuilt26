594481b8113f9ec62543e3f090c5d3feccf9410a
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-01-27 19:18:35 -0500

    feat: Add Intake subsystem with motor control, pivot positioning, and associated constants. rudimentry intake subsystem.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26'";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 17;
-  public static final String GIT_SHA = "79c83e65db03893f01da87d4a2b2cfaeaeaf3b99";
-  public static final String GIT_DATE = "2026-01-26 16:21:14 EST";
-  public static final String GIT_BRANCH = "Turret";
-  public static final String BUILD_DATE = "2026-01-26 19:55:54 EST";
-  public static final long BUILD_UNIX_TIME = 1769475354299L;
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "9bd15004ae0687c190442fa4c8e1d924afe00cbc";
+  public static final String GIT_DATE = "2026-01-27 16:13:35 EST";
+  public static final String GIT_BRANCH = "intake";
+  public static final String BUILD_DATE = "2026-01-27 19:08:51 EST";
+  public static final long BUILD_UNIX_TIME = 1769558931152L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import static frc.robot.Constants.SubsystemConstants.CANivore;
+import static frc.robot.Constants.SubsystemConstants.Intake.*;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+
+public class Intake extends SubsystemBase {
+  private final TalonFX intakeMotor;
+  private final TalonFX pivotMotor;
+  private final MotionMagicVoltage mmRequest = new MotionMagicVoltage(0);
+
+  /** Creates a new intake. */
+  public Intake() {
+    intakeMotor = new TalonFX(intakeMotorID, CANivore);
+    pivotMotor = new TalonFX(pivotMotorID, CANivore);
+    pivotMotor.getConfigurator().apply(pivotConfig);
+  }
+
+  public void setIntakeSpeed(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  public void startIntake() {
+    setIntakeSpeed(intakeSpeed);
+  }
+
+  public void stopIntake() {
+    setIntakeSpeed(0);
+  }
+
+  public void setPivotPosition(double position) {
+    pivotMotor.setControl(mmRequest.withPosition(position));
+  }
+
+  public void extend() {
+    pivotMotor.setControl(mmRequest.withPosition(downPos));
+  }
+
+  public void retract() {
+    pivotMotor.setControl(mmRequest.withPosition(upPos));
+  }
+}
