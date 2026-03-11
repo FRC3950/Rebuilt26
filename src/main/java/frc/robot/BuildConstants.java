8a51e48b621f408040f7e94ca15a01bf9e4443e2
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-11 10:08:03 -0400

    make voltage request with foc for better accelleration. buh.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 54;
-  public static final String GIT_SHA = "6063a6cf2790ac85c15593b7f40fb02826f9a6cc";
-  public static final String GIT_DATE = "2026-03-02 19:15:50 EST";
+  public static final int GIT_REVISION = 56;
+  public static final String GIT_SHA = "53551def4f5606be84ebb98275c24a0a9d44c4e2";
+  public static final String GIT_DATE = "2026-03-11 10:08:03 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-03 15:48:38 EST";
-  public static final long BUILD_UNIX_TIME = 1772570918445L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-11 10:08:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1773238091311L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
