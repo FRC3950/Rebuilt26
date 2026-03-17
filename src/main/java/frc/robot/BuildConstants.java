c6c89f3c30b185934451b95a588e330f21df0656
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-17 19:28:25 -0400

    bammm

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -9,13 +9,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "a23784ac10992f99a1535d21013180c21a1e189c";
   public static final String GIT_DATE = "2026-03-16 22:22:16 EDT";
   public static final String GIT_BRANCH = "main";
-<<<<<<< Updated upstream
   public static final String BUILD_DATE = "2026-03-17 19:21:10 EDT";
   public static final long BUILD_UNIX_TIME = 1773789670954L;
-=======
-  public static final String BUILD_DATE = "2026-03-16 22:31:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1773714668250L;
->>>>>>> Stashed changes
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
