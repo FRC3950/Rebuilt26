c4323c2e6fc2c059879c541d6d409e760c1cadcc
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-24 16:15:08 -0400

    updated drive foc status

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 115;
-  public static final String GIT_SHA = "b0f5cdf7b5f3ef6f53b160163e3bd3143873c655";
-  public static final String GIT_DATE = "2026-03-23 19:15:36 EDT";
+  public static final int GIT_REVISION = 116;
+  public static final String GIT_SHA = "2a8d08b35c9ad974bace3cee02b09300c480a905";
+  public static final String GIT_DATE = "2026-03-23 19:21:54 EDT";
   public static final String GIT_BRANCH = "advanced";
-  public static final String BUILD_DATE = "2026-03-23 19:20:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1774308045301L;
+  public static final String BUILD_DATE = "2026-03-24 15:42:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1774381354620L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
