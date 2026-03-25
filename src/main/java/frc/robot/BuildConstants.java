87011b2c9027965301564e94cba3eb305b30012f
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-25 17:17:16 -0400

    hood switchy

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 123;
-  public static final String GIT_SHA = "ac90e82bab0fbde4915c911984b44dbb44e56ee8";
-  public static final String GIT_DATE = "2026-03-25 16:21:09 EDT";
+  public static final int GIT_REVISION = 124;
+  public static final String GIT_SHA = "c870e3b38dd38d7dc59fcf7ac630a9ba76edc787";
+  public static final String GIT_DATE = "2026-03-25 16:26:22 EDT";
   public static final String GIT_BRANCH = "tune-2.0";
-  public static final String BUILD_DATE = "2026-03-25 16:21:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1774470087369L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-25 17:16:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1774473419371L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
