71fac95acb9e842c7cb050af5b9fb8bde83b7cd8
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-26 18:55:45 -0400

    sweepy auto works (at least getting fuel).

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 124;
-  public static final String GIT_SHA = "c870e3b38dd38d7dc59fcf7ac630a9ba76edc787";
-  public static final String GIT_DATE = "2026-03-25 16:26:22 EDT";
-  public static final String GIT_BRANCH = "tune-2.0";
-  public static final String BUILD_DATE = "2026-03-25 17:16:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1774473419371L;
+  public static final int GIT_REVISION = 123;
+  public static final String GIT_SHA = "7250e169f74ebabf760aaa5e62e0456905f93b86";
+  public static final String GIT_DATE = "2026-03-26 18:17:55 EDT";
+  public static final String GIT_BRANCH = "advanced";
+  public static final String BUILD_DATE = "2026-03-26 18:36:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1774564602954L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
