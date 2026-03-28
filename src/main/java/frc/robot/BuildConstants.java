ed03254320c1948d3aa7ed31579fda4ecee15467
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-28 15:05:44 -0400

    position in ascope config for limelight right is right I think???

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 133;
-  public static final String GIT_SHA = "126650a8a19d0c95966410e134f233b3a15c82f9";
-  public static final String GIT_DATE = "2026-03-28 14:51:55 EDT";
+  public static final int GIT_REVISION = 135;
+  public static final String GIT_SHA = "2856a7dc29c55e818bfebe2a55e7a6bd5d19afec";
+  public static final String GIT_DATE = "2026-03-28 14:58:04 EDT";
   public static final String GIT_BRANCH = "advanced";
-  public static final String BUILD_DATE = "2026-03-28 14:52:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1774723962998L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-28 14:58:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1774724311627L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
