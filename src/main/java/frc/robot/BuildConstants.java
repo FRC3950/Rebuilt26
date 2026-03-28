3a24c4d12c44986de59aad241a8cae9e72caa68f
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-28 14:54:56 -0400

    more limelight right.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 128;
-  public static final String GIT_SHA = "8febce3770a8218fe11a4bd09d4c392aebb97114";
-  public static final String GIT_DATE = "2026-03-28 13:04:19 EDT";
+  public static final int GIT_REVISION = 133;
+  public static final String GIT_SHA = "126650a8a19d0c95966410e134f233b3a15c82f9";
+  public static final String GIT_DATE = "2026-03-28 14:51:55 EDT";
   public static final String GIT_BRANCH = "advanced";
-  public static final String BUILD_DATE = "2026-03-28 14:18:12 EDT";
-  public static final long BUILD_UNIX_TIME = 1774721892142L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-28 14:52:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1774723962998L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
