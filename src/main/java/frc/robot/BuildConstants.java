cbda327c93909997c4b2ca86b088ac1d7b96d9d0
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-03-30 19:34:44 -0400

    Autos and edited some sim constants.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 144;
-  public static final String GIT_SHA = "d2b39121e3cc03b49fae0295b50df59104fe7ce7";
-  public static final String GIT_DATE = "2026-03-29 18:18:45 EDT";
+  public static final int GIT_REVISION = 145;
+  public static final String GIT_SHA = "0872bf920545efe71664a9fba9c9ae1efa502214";
+  public static final String GIT_DATE = "2026-03-29 18:36:12 EDT";
   public static final String GIT_BRANCH = "advanced";
-  public static final String BUILD_DATE = "2026-03-29 18:36:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1774823764709L;
+  public static final String BUILD_DATE = "2026-03-30 19:25:14 EDT";
+  public static final long BUILD_UNIX_TIME = 1774913114740L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
