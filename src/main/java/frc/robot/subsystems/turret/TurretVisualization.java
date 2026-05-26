eafb9284a300a3bff4b4cb29f670919d83818495
CJ Brandi <cj.brandi10@gmail.com>
2026-05-26 16:16:03 -0400

    fix turret logging poses.

--- src/main/java/frc/robot/subsystems/turret/TurretVisualization.java
+++ src/main/java/frc/robot/subsystems/turret/TurretVisualization.java
@@ -9,10 +9,13 @@ import frc.robot.Constants;
 import org.littletonrobotics.junction.Logger;
 
 public class TurretVisualization extends SubsystemBase {
+  private static final int REAL_LOG_PERIOD_LOOPS = 5;
   private static final Pose3d[] ZEROED_COMPONENT_POSES = {new Pose3d(), new Pose3d()};
 
   private final Turret turret1;
   private final Turret turret2;
+  private final Pose3d[] finalComponentPoses = new Pose3d[2];
+  private int loopCounter = 0;
 
   public TurretVisualization(Turret turret1, Turret turret2) {
     this.turret1 = turret1;
@@ -21,15 +24,18 @@ public class TurretVisualization extends SubsystemBase {
 
   @Override
   public void periodic() {
-    if (Constants.currentMode != Constants.Mode.REPLAY) {
+    boolean replay = Constants.currentMode == Constants.Mode.REPLAY;
+    boolean shouldLog = replay || loopCounter % REAL_LOG_PERIOD_LOOPS == 0;
+    loopCounter++;
+
+    if (!shouldLog) {
       return;
     }
 
+    finalComponentPoses[0] = turret1.getRobotPose3d(robotToTurret1);
+    finalComponentPoses[1] = turret2.getRobotPose3d(robotToTurret2);
+
     Logger.recordOutput("ZeroedComponentPoses", ZEROED_COMPONENT_POSES);
-    Logger.recordOutput(
-        "FinalComponentPoses",
-        new Pose3d[] {
-          turret1.getRobotPose3d(robotToTurret1), turret2.getRobotPose3d(robotToTurret2)
-        });
+    Logger.recordOutput("FinalComponentPoses", finalComponentPoses);
   }
 }
