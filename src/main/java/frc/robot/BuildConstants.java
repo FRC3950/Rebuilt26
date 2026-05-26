b278c49c917e54fcf3212a207d051aa647875e9c
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-05-26 16:50:46 -0400

    vs code and build.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 180;
-  public static final String GIT_SHA = "65bad6c11957075868b3e177b80cd65e522bac0f";
-  public static final String GIT_DATE = "2026-05-20 22:11:04 EDT";
+  public static final int GIT_REVISION = 185;
+  public static final String GIT_SHA = "eafb9284a300a3bff4b4cb29f670919d83818495";
+  public static final String GIT_DATE = "2026-05-26 16:16:03 EDT";
   public static final String GIT_BRANCH = "offseason";
-  public static final String BUILD_DATE = "2026-05-22 15:28:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1779478128684L;
+  public static final String BUILD_DATE = "2026-05-26 16:41:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1779828093857L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
