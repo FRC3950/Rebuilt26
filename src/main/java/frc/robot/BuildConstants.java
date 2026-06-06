29825ee93e463c15cb397254d9d57ec9702755b4
DriverStation3950 <northshorerobotics3950@gmail.com>
2026-06-05 20:37:31 -0400

    depot auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 193;
-  public static final String GIT_SHA = "cfc3f0b45248195e6fb0c2281c8bcbfd4d5c1cf3";
-  public static final String GIT_DATE = "2026-06-05 13:10:02 EDT";
+  public static final int GIT_REVISION = 195;
+  public static final String GIT_SHA = "d6f5e7f1a4b8c9e5390cea0dc20efb82f52880d2";
+  public static final String GIT_DATE = "2026-06-05 16:11:49 EDT";
   public static final String GIT_BRANCH = "battlecry";
-  public static final String BUILD_DATE = "2026-06-05 13:28:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1780680493115L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-06-05 19:46:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1780703202731L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
